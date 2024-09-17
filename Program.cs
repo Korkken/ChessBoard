611b5c2b6eef3e82f88624ba6c2735943f664c33
timnh <timnh@PC2CYEVA>
2024-09-17 14:27:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Välj en siffra");
+
+int.TryParse(Console.ReadLine(), out int number);
+
+
+for (int t = 0; t < number; t++)
+{
+
+    for (int i = 0; i < number; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.Write("\u25A0 ");
+        }
+        else 
+        {
+            Console.Write("\u25A1 ");
+        }
+    }
+    Console.WriteLine();
+
+}
+
