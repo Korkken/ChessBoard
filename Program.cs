7b9303bb08a8af1d3ec0820fce8006f6e6667e00
Tim Lappalainen <timnhl95@hotmail.com>
2024-09-20 13:42:58 +0200

    Added loop to check if user wrote a number

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.WriteLine("Välj en siffra");
+int number;
 
-int.TryParse(Console.ReadLine(), out int number);
-
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("Skriv en siffra eller ett tal, tack!");
+}
 
 for (int t = 0; t < number; t++)
 {
