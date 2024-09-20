0cdd0dd51b458ef560084b2e1d0139287515821e
Tim Lappalainen <timnhl95@hotmail.com>
2024-09-20 15:21:22 +0200

    Added ability to choose what the squares should be

--- Program.cs
+++ Program.cs
@@ -3,23 +3,28 @@ Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.WriteLine("Välj en siffra");
 int number;
 
-while (!int.TryParse(Console.ReadLine(), out number))
+while (!int.TryParse(Console.ReadLine(), out number)) //checking if value is a number
 {
     Console.WriteLine("Skriv en siffra eller ett tal, tack!");
 }
 
+Console.WriteLine("Vad för tecken vill du att vit ska vara"); //user chooses what the white squares should be
+var white = Console.ReadLine();
+
+Console.WriteLine("Vad för tecken vill du att svart ska vara"); //user chooses what the black squares should be
+var black = Console.ReadLine();
 for (int t = 0; t < number; t++)
 {
 
-    for (int i = 0; i < number; i++)
+    for (int i = 0; i < number; i++) //loop to print out the chessboard.
     {
         if ((i + t) % 2 == 0 )
         {
-            Console.Write("\u25A0 ");
+            Console.Write(white);
         }
         else 
         {
-            Console.Write("\u25A1 ");
+            Console.Write(black);
         }
     }
     Console.WriteLine();
