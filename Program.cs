97c9852e1889e28098d94b3dbaeb2226fa6610c8
Korkken <Korkken@gmail.com>
2024-09-20 15:57:35 +0200

    added input encoding

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//Tim Lappalainen NET2024
 Console.OutputEncoding = System.Text.Encoding.Unicode;
-Console.WriteLine("Välj en siffra");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Hur stort bräde vill du ha?");
 int number;
 
 while (!int.TryParse(Console.ReadLine(), out number)) //checking if value is a number
@@ -8,23 +9,23 @@ while (!int.TryParse(Console.ReadLine(), out number)) //checking if value is a n
     Console.WriteLine("Skriv en siffra eller ett tal, tack!");
 }
 
-Console.WriteLine("Vad för tecken vill du att vit ska vara"); //user chooses what the white squares should be
+Console.WriteLine("Vad för tecken vill du att vita rutorna ska vara"); //user chooses what the white squares should be
 var white = Console.ReadLine();
 
-Console.WriteLine("Vad för tecken vill du att svart ska vara"); //user chooses what the black squares should be
+Console.WriteLine("Vad för tecken vill du att svarta rutorna ska vara"); //user chooses what the black squares should be
 var black = Console.ReadLine();
 for (int t = 0; t < number; t++)
 {
 
     for (int i = 0; i < number; i++) //loop to print out the chessboard.
     {
-        if ((i + t) % 2 == 0 )
+        if ((i + t) % 2 == 0)
         {
-            Console.Write(white);
+            Console.Write($"{white} ");
         }
         else 
         {
-            Console.Write(black);
+            Console.Write($"{black} ");
         }
     }
     Console.WriteLine();
