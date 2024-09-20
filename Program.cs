e760d26b40f0c567511a5b9e244ca1c9cba4de4b
Tim Lappalainen <timnhl95@hotmail.com>
2024-09-20 14:56:12 +0200

    Added name and class name to top.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Tim Lappalainen NET2024
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.WriteLine("Välj en siffra");
 int number;
